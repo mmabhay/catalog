f41d73f764a864d0483b2b4600e5d136c45a5722
Abhay Chauhan <abhay.chauhan_1995@hotmail.com>
2016-12-29 00:54:42 +0530

    Add restaurants and menu items template

--- finalproject.py
+++ finalproject.py
@@ -11,11 +11,26 @@ Base.metadata.bind = engine
 DBSession = session_maker(bind = engine)
 session = DBSession()
 
+# This module will display all the restaurants
 @app.route('/')
-@app.route('/restaurant')
+@app.route('/restaurants/')
 def showRestaurants():
     restaurants = session.query(Restaurant).all()
     return render_template('restaurants.html', restaurants = restaurants)
+
+# This module will display all the menu items
+@app.route('/menuitems/')
+def showAllMenuItems():
+    menuitems = session.query(MenuItem).all()
+    return render_template('menuitems.html', menuitems = menuitems)
+
+@app.route('/editrestaurant/<int:restaurant_id>', methods = ['GET','POST'])
+def editRestaurant(restaurant_id):
+    if request.method == 'POST':
+
+
+
+
 
 if __name__ == '__main__':
     # secret_key will be used for session
